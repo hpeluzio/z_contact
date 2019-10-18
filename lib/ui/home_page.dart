e3bf5c08c0565523c114aad2ff6b33e96f42e119
Henrique Peluzio <hpeluzio@gmail.com>
2019-10-18 16:45:10 -0300

    OrderAz-Za finalizado

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -4,6 +4,8 @@ import 'package:z_contact/ui/contact_page.dart';
 import 'dart:io';
 import 'package:url_launcher/url_launcher.dart';
 
+enum OrderOptions {orderaz, orderza}
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -40,6 +42,21 @@ class _HomePageState extends State<HomePage> {
         title: Text('Contatos'),
         backgroundColor: Colors.red,
         centerTitle: true,
+        actions: <Widget>[
+          PopupMenuButton<OrderOptions>(
+            itemBuilder: (context) => <PopupMenuEntry<OrderOptions>>[
+              const PopupMenuItem<OrderOptions>(
+                child: Text('Order de A-Z'),
+                value: OrderOptions.orderaz,
+              ),
+              const PopupMenuItem<OrderOptions>(
+                child: Text('Order de Z-A'),
+                value: OrderOptions.orderza,
+              ),
+            ],
+            onSelected: _orderList, 
+          )
+        ],
       ),
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
@@ -220,7 +237,23 @@ class _HomePageState extends State<HomePage> {
     });    
   }
 
-
+  void _orderList(OrderOptions result) {
+    switch(result) {
+      case OrderOptions.orderaz:
+        contacts.sort((a,b) {
+          return a.name.toLowerCase().compareTo(b.name.toLowerCase());
+        });
+        break;
+      case OrderOptions.orderza:
+        contacts.sort((a,b) {
+          return b.name.toLowerCase().compareTo(a.name.toLowerCase());
+        });      
+        break;
+    }
+    setState(() {
+      
+    });
+  }
 
 
 }
